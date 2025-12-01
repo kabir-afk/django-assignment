72825b2f071cf1a79eb90f969bc7855fb7a6f8d5
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-02 00:34:44 +0530

    chore : added settings for pagination

--- banks/settings.py
+++ banks/settings.py
@@ -78,6 +78,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'banks.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
