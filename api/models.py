91b779c12cd2de12279952951d7b5b28cafdf23b
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-01 22:09:18 +0530

    update : added pk to both bank and branch models

--- api/models.py
+++ api/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Bank(models.Model):
-    id = models.BigIntegerField(primary_key=True , null=False)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=49)
     class Meta:
         db_table = 'banks'
 class Branch(models.Model):
-    bank = models.ForeignKey(Bank,on_delete=models.CASCADE,related_name="branch")
-    ifsc = models.CharField(max_length=11,null=False)
-    name = models.CharField(max_length=74)
+    ifsc = models.CharField(max_length=11,primary_key=True)
+    bank = models.ForeignKey(Bank,on_delete=models.CASCADE,related_name="branch",db_column="bank_id")
+    name = models.CharField(max_length=74,db_column="branch")
     address = models.CharField(max_length=195)
     city = models.CharField(max_length=50)
     district = models.CharField(max_length=50)
