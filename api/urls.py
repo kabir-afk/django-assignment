bfaeb9e24cb1aaaee2f54762cc61fca7241d4c89
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-01 22:08:15 +0530

    update : added branches url route

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Banks
+from .views import Banks , Branches
 
 urlpatterns = [
     path('', Banks.as_view()),
+    path('branches', Branches.as_view()),
 ]
