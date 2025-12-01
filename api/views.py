56b1cf52d32fde32767b0c88e7fc4f2ddfcdc702
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-02 00:35:36 +0530

    feat : implemented pagination when fetching all branches at once

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import status
+from rest_framework import status , generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from .models import Bank, Branch
 from .serializers import BankSerializer,BranchSerializer
 
@@ -10,7 +11,12 @@ class Banks(APIView):
         banks = Bank.objects.all()
         serializer = BankSerializer(banks ,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
-class Branches(APIView):
+class All_bank_branches(generics.ListAPIView):
+    pagination_class = PageNumberPagination
+    queryset = Branch.objects.order_by('pk')
+    serializer_class = BranchSerializer
+    
+class Branch_by_IFSC_code(APIView):
     def get(self, req,ifsc):
         branches = Branch.objects.get(ifsc=ifsc)
         serializer = BranchSerializer(branches)
