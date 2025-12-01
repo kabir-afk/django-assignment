5b49224a155013a7d4c4ecd304c20613b71e631b
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-02 00:34:07 +0530

    feat : added route for fetching all branches at once
    update : view function names

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Banks , Branches
+from .views import Banks , All_bank_branches ,Branch_by_IFSC_code
 
 urlpatterns = [
     path('', Banks.as_view()),
-    path('branches/<str:ifsc>', Branches.as_view()),
+    path('branches', All_bank_branches.as_view()),
+    path('branches/<str:ifsc>', Branch_by_IFSC_code.as_view()),
 ]
