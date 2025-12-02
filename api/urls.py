76735ff755a3d4c6df3ca9043d40faea4c938246
Kabir Singh <kabirsingh12b@gmail.com>
2025-12-02 16:44:45 +0530

    chore : added routes for fetching specific banks' states and cities

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BankListView,BankDetailView,BankBranchListView,BranchListView,BranchDetailView , BranchValidateView
+from .views import BankListView,BankCitiesListView,BankStateListView,BankDetailView,BankBranchListView,BranchListView,BranchDetailView , BranchValidateView
 
 urlpatterns = [
     path('banks', BankListView.as_view()),
     path('banks/<int:id>', BankDetailView.as_view()),
+    path('banks/<int:id>/cities', BankCitiesListView.as_view()),
+    path('banks/<int:id>/states', BankStateListView.as_view()),
     path('banks/<int:id>/branches', BankBranchListView.as_view()),
     path('branches', BranchListView.as_view()),
     path('branches/<str:ifsc>', BranchDetailView.as_view()),
